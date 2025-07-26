b717ed4d97e0265bbe3112c1437e1ba957adcc7d
dulatovazh <dulatovazh@gmail.com>
2025-07-26 13:01:09 +0300

    HW5

--- js/lesson.js
+++ js/lesson.js
@@ -26,3 +26,62 @@ setInterval(() => {
     }
     showTabContent(currentIndex);
 }, 3000);
+
+const somInput = document.querySelector('#som');
+const usdInput = document.querySelector('#usd');
+const eurInput = document.querySelector('#eur');
+
+let isUpdating = false;
+
+somInput.oninput = () => {
+    if (isUpdating) return;
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', '../data/converter.json');
+    xhr.setRequestHeader('Content-type', 'application/json');
+    xhr.send();
+
+    xhr.onload = () => {
+        const conv = JSON.parse(xhr.response);
+        isUpdating = true;
+        usdInput.value = (somInput.value / conv.usd).toFixed(2);
+        eurInput.value = (somInput.value / conv.eur).toFixed(2);
+        isUpdating = false;
+    };
+};
+
+usdInput.oninput = () => {
+    if (isUpdating) return;
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', '../data/converter.json');
+    xhr.setRequestHeader('Content-type', 'application/json');
+    xhr.send();
+
+    xhr.onload = () => {
+        const conv = JSON.parse(xhr.response);
+        const som = usdInput.value * conv.usd;
+        isUpdating = true;
+        somInput.value = som.toFixed(2);
+        eurInput.value = (som / conv.eur).toFixed(2);
+        isUpdating = false;
+    };
+};
+
+eurInput.oninput = () => {
+    if (isUpdating) return;
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', '../data/converter.json');
+    xhr.setRequestHeader('Content-type', 'application/json');
+    xhr.send();
+
+    xhr.onload = () => {
+        const conv = JSON.parse(xhr.response);
+        const som = eurInput.value * conv.eur;
+        isUpdating = true;
+        somInput.value = som.toFixed(2);
+        usdInput.value = (som / conv.usd).toFixed(2);
+        isUpdating = false;
+    };
+};
+
+
+
